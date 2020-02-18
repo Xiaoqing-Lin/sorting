c5be91cedf7a50b32a2036505e63437d64e460d3
Xiaoqing-Lin <57362207+Xiaoqing-Lin@users.noreply.github.com>
2020-02-18 16:47:19 -0500

    working on mergesort

--- mergesort.js
+++ mergesort.js
@@ -1,41 +1,51 @@
+/* eslint-disable no-unused-vars */
 function split(wholeArray) {
-
   /* your code here to define the firstHalf and secondHalf arrays */
 
   if (wholeArray.length === 1) {
     return wholeArray;
   }
 
-  const half = Math.ceil(wholeArray.length/2);
-  const firstHalf = wholeArray.slice(0, half)
+  const half = Math.ceil(wholeArray.length / 2);
+  const firstHalf = wholeArray.slice(0, half);
   const secondHalf = wholeArray.slice(half);
-
 
   return [firstHalf, secondHalf];
 }
 
 function merge(firstArr, secondArr) {
-
-  let sortedArray= [];
-
-  for (let i=0; i< secondArr.length; i++) {
-    if (firstArr[i] < secondArr[i]){
-      sortedArray.push(firstArr[i], secondArr[i])
-
+  let sortedArray = [];
+  while (secondArr.length) {
+    if (firstArr[0] < secondArr[0]) {
+      sortedArray.push(firstArr[0]);
+      firstArr.shift();
     } else {
-      sortedArray.push(secondArr[i], firstArr[i]);
+      sortedArray.push(secondArr[0]);
+      secondArr.shift();
     }
   }
 
+  if (firstArr.length) {
+    sortedArray = [...sortedArray, ...firstArr];
+  }
   return sortedArray;
 }
 
-
 function mergeSort(array) {
-  //keeps splitting array until only array size = 1
+  // keeps splitting array until only array size = 1
+  const splitArray = split(array);
+  // will continue to split() until the most-nested-array.length is 1
+  let first=splitArray[0];
+  let second=splitArray[1];
+  console.log('splitarray', splitArray)
+  while (splitArray[0].length > 1) {
+    first = mergeSort(splitArray[0]);
+    second = mergeSort(splitArray[1]);
+  }
+  console.log('first:', first)
+  console.log('second:', second)
 
+  return merge(first, second);
 
-
-  //calls merge until all arrays become 1 again
-
+  // calls merge until all arrays become 1 again
 }
--- mergesort.spec.js
+++ mergesort.spec.js
@@ -1,24 +1,29 @@
+/* eslint-disable no-undef */
 describe('Split Array function', function() {
   it('is able to split an even array into two halves', function() {
-    expect( split([1,2,3,4])).toEqual([[1,2],[3,4]]);
-    expect( split([1,2])).toEqual([[1],[2]]);
+    expect(split([1, 2, 3, 4])).toEqual([[1, 2], [3, 4]]);
+    expect(split([1, 2])).toEqual([[1], [2]]);
   });
 
   it('is able to split an odd array into two halves', function() {
-    expect( split([1,2,3,4,5])).toEqual([[1,2,3],[4,5]]);
-    expect( split([1,2,3])).toEqual([[1,2],[3]]);
+    expect(split([1, 2, 3, 4, 5])).toEqual([[1, 2, 3], [4, 5]]);
+    expect(split([1, 2, 3])).toEqual([[1, 2], [3]]);
   });
 });
 
-
-
-describe('Merge function', function(){
-  it('is able to merge two sorted arrays into one sorted array', function(){
-    expect( merge([1],[2])).toEqual([1,2]);
-    expect( merge([1,2],[3,4])).toEqual([1,2,3,4]);
+describe('Merge function', function() {
+  it('is able to merge two sorted arrays into one sorted array', function() {
+    expect(merge([1], [2])).toEqual([1, 2]);
+    expect(merge([1, 2], [3, 4])).toEqual([1, 2, 3, 4]);
+    expect(merge([1, 2, 3], [4, 5])).toEqual([1, 2, 3, 4, 5]);
   });
+});
 
-
+describe('mergeSort function', function() {
+  it('is able to split and merge two sorted arrays into one sorted array', function() {
+    expect(mergeSort([2, 1])).toEqual([1, 2]);
+    expect(mergeSort([1, 2, 3, 4])).toEqual([1, 2, 3, 4]);
+  });
 });
 
 /*
